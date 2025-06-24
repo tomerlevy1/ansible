2e996b5f486a7c85a6f3320e8887f64a7b4f77b7
Tomer <tolevy@outbrain.com>
2025-06-24 12:12:18 +0300

    feat(setup): create main setup.sh script

---
+++ mac-setup/setup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Main orchestrator script for mac-setup
+
+# Source utility functions
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source "$SCRIPT_DIR/lib/utils.sh"
+
+# Further logic will be added in subsequent steps. 
